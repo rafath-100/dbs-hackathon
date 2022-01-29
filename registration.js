395b534f0891b0a3a0805d07d75dec0c4cc4e3a3
bhavana7737 <79309501+bhavana7737@users.noreply.github.com>
2022-01-29 13:08:48 +0530

    Add files via upload

---
+++ registration.js
@@ -0,0 +1,21 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+function formValidation() {
+  if (name.value.length < 2 || name.value.length > 20) {
+    alert("Name length should be more than 2 and less than 21");
+    name.focus();
+    return false;
+  }
+  if (email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    alert("Please enter a valid email!");
+    email.focus();
+    return false;
+  }
+  if (!password.value.match(/^.{5,15}$/)) {
+    alert("Password length must be between 5-15 characters!");
+    password.focus();
+    return false;
+  }
+  return true;
+}
